2980875bf48264e0863e0a107a19ad3633d24b8f
Indhumini <47097062+indhumini@users.noreply.github.com>
2020-05-06 20:13:02 +0530

    front page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.productService.showMessage();
+    
   }
+
 
 }
